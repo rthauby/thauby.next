f907bce3207b527b0c6f6761420c7ace163f3afe
Rodrigo Thauby <rthauby@Rodrigos-MacBook-Pro.local>
2023-05-31 16:37:09 -0400

    fixes

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -49,13 +49,13 @@ class Intro extends Component {
               <p>Through it all, my interest has always been in working as close to the user as possible. Working on clients has the distinct benefit to see the results of your work immediately, and to see the impact that every decision has over the User Experience.</p>
             </div>
 
-            <div class="bg-white py-24 space-y-12">
-              <div class="mx-auto max-w-7xl px-6 lg:px-8 bg-slate-200 rounded sm:py-8">
-                <h2 class="text-center font-semibold leading-8 text-gray-900">These are some of the technologies and platforms I&apos;m enjoying using right now:</h2>
-                <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+            <div className="bg-white py-24 space-y-12">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8 bg-slate-200 rounded sm:py-8">
+                <h2 className="text-center font-semibold leading-8 text-gray-900">These are some of the technologies and platforms I&apos;m enjoying using right now:</h2>
+                <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
                   {logos.map((logo, i) => {
                     return (
-                      <Image key={i} class="col-span-2 max-h-24 w-full object-contain lg:col-span-1" src={logo.url} alt={logo.alt} width="158" height="128" />
+                      <Image key={i} className="col-span-2 max-h-24 w-full object-contain lg:col-span-1" src={logo.url} alt={logo.alt} width="158" height="128" />
                     )
                   })}
                 </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Rodrigo Thauby</title>
       <Head />
       <body>
         <Main />
