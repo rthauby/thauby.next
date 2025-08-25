d69b61a435a44f36c984385b018b20752f00cccb
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-08-25 14:21:01 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
-      <h2 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Hi,</h2>
-      <div className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-        <h1 className='pb-8'>I&#39;m Rodrigo Thauby.</h1>
-        <h2>A Programmer, Nerd, and Dad, living in Carlisle, PA.</h2>
+    <div className='h-[50vh] mx-auto max-w-2xl my-[25vh] flex flex-col justify-center'>
+      <div className="">
+        <h2 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Hi,</h2>
+        <div className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+          <h1 className='pb-8'>I&#39;m Rodrigo Thauby.</h1>
+          <h2>A Programmer, Nerd, and Dad, living in Carlisle, PA.</h2>
+        </div>
       </div>
+
     </div>
   );
 }
