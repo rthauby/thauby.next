1e8a7d14897f195fc8c76fc40eac7da71e5cfe9b
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-08-25 12:31:46 -0400

    Squashed commit of the following:
    commit 9ce91ec8d5de5d6cfe06708052cd3e6eb8a5a900
    Author: Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
    Date:   Mon Aug 25 12:31:27 2025 -0400
    
        fuck it. works i guess
    
    commit 4f751590c0630eb100c4e6a6970741c4a7a9bee3
    Author: Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
    Date:   Mon Aug 25 11:58:33 2025 -0400
    
        wipe

--- src/pages/history/sparkypants.tsx
+++
@@ -1,9 +0,0 @@
-import CVDetail from '@/components/CVDetail'
-// @ts-ignore
-import sparkypants from '@/md/sparkypants.md'
-
-export default function Page() {
-  return (
-    <CVDetail title='Sparkypants' img='/img/jdImages/sparkypants.jpg' body={sparkypants} />
-  )
-}
--- src/pages/history/virtru.tsx
+++
@@ -1,9 +0,0 @@
-import CVDetail from '@/components/CVDetail'
-// @ts-ignore
-import virtru from '@/md/virtru.md'
-
-export default function Page() {
-  return (
-    <CVDetail title='Virtru' img='/img/jdImages/virtru.jpg' body={virtru} />
-  )
-}
