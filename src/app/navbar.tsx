ea90b1213f257fe450c6e9e1c438164c05f9f3cf
Rodrigo Thauby <rthauby@gmail.com>
2025-08-26 15:22:03 -0400

    Replace anchor tags with Link components

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 export default function NavBar() {
   return (
     <nav className="fixed w-full pointer-events-none border-b border-blue-950">
       <div className="mx-auto w-full max-w-2xl xl:max-w-5xl">
         <div className="p-4 flex justify-between items-center justify-end">
           <div className="space-x-8 pointer-events-auto text-2xl">
-            <a href="/" className="hover:underline">Home</a>
-            <a href="/about" className="hover:underline">About Me</a>
+            <Link href="/" className="hover:underline">Home</Link>
+            <Link href="/about" className="hover:underline">About Me</Link>
           </div>
         </div>
       </div>
