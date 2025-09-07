7fb7a25ad943e9fa3b6a640aacd1a72d0e91b50b
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-09-06 21:25:06 -0400

    wer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["thauby.com"],
+  },
 };
 
 export default nextConfig;
