786e5edf94045a97e8546ffa9bf57c3047828d30
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-09-10 20:29:54 -0400

    Revert "try this"
    This reverts commit 6daa5889252a8aae0a439beb273d42aa045a3b7f.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_WEBFORMS_API_KEY: process.env.NEXT_PUBLIC_WEBFORMS_API_KEY,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
