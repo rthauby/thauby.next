4f4f541bad5e99578567c561ee478e929fa4d0a4
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-09-10 20:11:06 -0400

    try this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} px-4 lg:px-0 antialiased overflow-x-hidden`}>
         <NavBar />
         {children}
-        <Contact access_key={process.env.WEBFORMS_API_KEY} />
+        <Contact access_key={process.env.NEXT_PUBLIC_WEBFORMS_API_KEY} />
         <Footer />
       </body>
     </html>
