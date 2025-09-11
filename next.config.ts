6daa5889252a8aae0a439beb273d42aa045a3b7f
Rodrigo Thauby <rodrigo.thauby@boulevardcg.com>
2025-09-10 20:26:43 -0400

    try this

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_WEBFORMS_API_KEY: process.env.NEXT_PUBLIC_WEBFORMS_API_KEY,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
